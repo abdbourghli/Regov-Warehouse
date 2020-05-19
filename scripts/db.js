8e6388eb54c45a7d15a86a9fdb5a33b6c608dc4f
Abdulrazzak Bourghli <abdbourghli@gmail.com>
2020-05-19 22:08:16 +0800

    add a function to fetch users hashed password from vault

--- scripts/db.js
+++ scripts/db.js
@@ -40,22 +40,13 @@ exports.addUserToDB = async user=>{
         catch(e){
             console.log("Error accured during hash commit, Removing user record",e)
             //delete the new row from the user table
-            var deleteUserQuery = `DELETE FROM user WHERE email = '${user.email}'`;
+            var deleteUserQuery = `DELETE FROM users WHERE email = '${user.email}'`;
             con.query(deleteUserQuery, function (err, result) {
                 if (err) throw err;
                 console.log("Number of records deleted: " + result.affectedRows);
             });
         }
     }
-  });
-}
-
-//adding hash value to the passVault table and link it with the user id
-const commitUserPass = (userID, userHash)=>{
-    const inserUserQuery = `INSERT INTO passVault (userID, passHash) VALUES 
-    (${userID}, "${userHash}")`;
-    con.query(inserUserQuery, function (err, result) {
-    if (err) throw err;
   });
 }
 
@@ -115,6 +106,22 @@ exports.getAllUsers = ()=>{
           });
     })
 }
+
+//get user password hash
+exports.getUserHash = id=>{
+    return new Promise((resolve,reject)=>{
+        con.query(`SELECT passHash FROM passVault WHERE userID = "${id}"`, function (err, result, fields) {
+            if (err) reject (err);
+            if(result[0]){
+                resolve (result[0])
+            }
+            else {
+                resolve (null)
+            }
+          });
+    })
+}
+
 //Conect to DB
 exports.connect = ()=>{con.connect(function(err) {
     if (err) throw err;
