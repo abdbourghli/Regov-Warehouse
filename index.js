a37a3200ab33b5e5511a0f3063ef797626720ce9
Abdulrazzak Bourghli <abdbourghli@gmail.com>
2020-05-19 19:10:54 +0800

    create endpoint for user login

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express'),
     app = express(),
     path = require('path'),
-    bodyParser = require('body-parser'),
-    user = require('./endpoints/user')
+    bodyParser = require('body-parser')
 
 
 // use enviroment port in production or port 5000 for local development or
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname + "/")))
+// app.use(express.static(path.join(__dirname + "/")))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // bodyParser middleware for parsing JSON body
@@ -16,4 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 //user endpints
-app.get('/users', user.list);
+// app.get('/users', user.list);
+// app.post('/login', user.login)
+
+
+var base = require("./routes/base");
+app.use(base);
--- routes/base.js
+++ routes/base.js
@@ -1,20 +1,37 @@
+var express = require('express')
+var router = express.Router()
+
+//Test data
 var users = [
     { name: 'test1', email: 'test1@tex.com' },
     { name: 'test2', email: 'test2@tex.com' },
     { name: 'test3', email: 'test3@tex.com' }
   ];
   
-  exports.list = function(req, res){
-    res.send({ title: 'Users', users: users });
-  };
 
-  exports.login = function(req, res){
+
+//Home
+router.get('/', function (req, res) {
+  res.send('Home page')
+})
+
+/// User endpoints ///
+
+//get list of users
+router.get('/users', function (req, res) {
+    res.send({ title: 'Users', users: users });
+})
+
+//Login
+router.post('/login', function (req, res) {
     let userIndex = users.findIndex(x => x.name ===req.body.name);
     if (userIndex>-1) {
-      next();
+      res.send(users[userIndex])
     } else {
       var err = new Error('cannot find user ' + req.body.name);
       res.status = 404;
       res.send(err)
     }
-  };
+})
+
+module.exports = router
