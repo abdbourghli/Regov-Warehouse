beded1b2e8bcbbd46f10a63fd58da323678aed44
Abdulrazzak Bourghli <abdbourghli@gmail.com>
2020-05-19 20:43:09 +0800

    add new user to database

--- scripts/db.js
+++ scripts/db.js
@@ -50,6 +50,15 @@ exports.addUserToDB = async user=>{
   });
 }
 
+//adding hash value to the passVault table and link it with the user id
+const commitUserPass = (userID, userHash)=>{
+    const inserUserQuery = `INSERT INTO passVault (userID, passHash) VALUES 
+    (${userID}, "${userHash}")`;
+    con.query(inserUserQuery, function (err, result) {
+    if (err) throw err;
+  });
+}
+
 //Conect to DB
 exports.connect = ()=>{con.connect(function(err) {
     if (err) throw err;
