5bef6d609ea6eb5c70d50b92f94bcc0adac56d95
Abdulrazzak Bourghli <abdbourghli@gmail.com>
2020-05-19 20:34:50 +0800

    add query to add password hash to passVault table

--- scripts/db.js
+++ scripts/db.js
@@ -8,7 +8,17 @@ const con = mysql.createConnection({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
   });
-  
+
+//adding hash value to the passVault table and link it with the user id
+const commitUserPass = (userID, userHash)=>{
+    const inserUserQuery = `INSERT INTO passVault (userID, passHash) VALUES 
+    (${userID}, "${userHash}")`;
+    con.query(inserUserQuery, function (err, result) {
+    if (err) throw err;
+  });
+}
+
+
 //Conect to DB
 exports.connect = ()=>{con.connect(function(err) {
     if (err) throw err;
